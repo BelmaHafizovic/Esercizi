b7cb88c86e7339a495eb74fcf0ddb2f66261eeaa
Belma <belmahfz@hotmail.com>
2023-03-22 09:56:03 +0100

    non funziona - prova

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public static void main (String[] args) throws java.lang.Exception
+        {
+        String[] words = {"pop","bat","tab","opp"};
+        List<List<String>> res = groupAnagrams(words);
+        System.out.println(res);
+        }
+
+        }
